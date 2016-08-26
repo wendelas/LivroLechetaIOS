b46ea361f903ee731d4a2c2647460da3587dcf24
wendelas <wendelas@gmail.com>
2016-08-26 05:39:43 -0700

    Initial Commit

---
+++ OlaObjectiveC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OlaObjectiveC
+//
+//  Created by Wendel de Assis da Silva on 8/26/16.
+//  Copyright © 2016 Livros Sa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
